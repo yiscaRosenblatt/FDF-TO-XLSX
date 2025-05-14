9d2f1f6c8e54dd46650a635d01150a21a4b24eda
yiscaRosenblatt <yisca.rose@gmail.com>
2025-05-14 22:49:23 +0300

    כל תשובה נימצאת בתא אחר בxlsx

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from  PDFTOXLSX import pdf_to_xlsx
+from PDFTOXLSX import pdf_to_xlsx
 
 if __name__ == "__main__":
     pdf_to_xlsx()
--- PDFTOXLSX.py
+++ PDFTOXLSX.py
@@ -10,7 +10,7 @@ class pdf_to_xlsx:
 
     def conversion(self):
         pdf_file_path = r"C:\Users\yisca\Desktop\BetaYeda\מאגר מסכם לשאלות בטיחות ועזרה ראשונה_220531_195438.pdf"
-        excel_file_path = r"C:\Users\yisca\Desktop\BetaYeda\questionsInELSX.xlsx"
+        excel_file_path = r"C:\Users\yisca\Desktop\BetaYeda\Option.xlsx"
 
         QuestionID = []
         Description = []
@@ -20,7 +20,7 @@ class pdf_to_xlsx:
         Option4 = []
         current_answer = ""
         inside_answer = False
-
+        current_option = ""
 
         with pdfplumber.open(pdf_file_path) as pdf: #פתיחת קובץ
             for page in pdf.pages: #מעבר על כל העמודים
@@ -30,25 +30,37 @@ class pdf_to_xlsx:
                     for line in lines:
                         cleaned_line = line.strip()
                         if cleaned_line:
-                            options = re.split(r'(\s*[א-ד]\s*\.\s*)', cleaned_line) #מחזיר רשימה
-                            current_option = ""
-                            for option in options:
-                                if re.match(r'(\s*[א-ד]\s*\.\s*)', option):
-                                    if current_option.strip():
-                                        reshaped = arabic_reshaper.reshape(current_option.strip()) #מתקן שהאותיות יופיעו נכון
-                                        fixed_line = get_display(reshaped) #3יצייג את כיוון הטקסט מימין לשמאול
-                                        QuestionID.append([fixed_line]) #מויסף את התשובה
-                                    current_option = option
-                                else:
-                                    current_option += " " + cleaned_line  # המשך של אותה תשובה
+                            # if re.match(r'([א-ד]\s*\.\s*)', cleaned_line):
+                            #     current_option = ""
+                            #     current_option = cleaned_line
+                            # else:
+                            #     current_option += cleaned_line
+                            #
                             # if current_option.strip():
                             #     reshaped = arabic_reshaper.reshape(current_option.strip())
                             #     fixed_line = get_display(reshaped)
                             #     QuestionID.append([fixed_line])
-                                # current_option = ""
+                            # options = re.split(r'([א-ד]\s*\.\s*)', cleaned_line) #מחזיר רשימה
+                            options = re.split(r'\s\.', cleaned_line)
+                            current_option = ""
+                            if len(options) == 2 and options[0] != "":
+                                current_answer = options[0]
+                            elif len(options) == 1:
+                                current_answer = cleaned_line + current_answer
+
+                            if re.match(r'\.', current_answer):
+                                reshaped = arabic_reshaper.reshape(current_answer.strip()) #מתקן שהאותיות יופיעו נכון
+                                fixed_line = get_display(reshaped) #3יצייג את כיוון הטקסט מימין לשמאול
+                                QuestionID.append([fixed_line]) #מויסף את התשובה
+
+                            # if current_option.strip():
+                            #     reshaped = arabic_reshaper.reshape(current_option.strip())
+                            #     fixed_line = get_display(reshaped)
+                            #     QuestionID.append([fixed_line])
 
         df = pd.DataFrame(QuestionID, columns=["Description"])
         df.to_excel(excel_file_path, index=False)
+
 
         print("עובד")
 
